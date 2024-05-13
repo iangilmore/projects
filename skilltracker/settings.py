3ecd3c2eb207d0bb2c398777e1c62671be67252c
Ian Gilmore <ian.gilmore@me.com>
2024-05-13 15:13:16 -0400

    update browsable API bootstrap theme

--- skilltracker/settings.py
+++ skilltracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_redesign',
     'rest_framework',
     'main_app',
 ]
