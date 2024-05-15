23fdaeeea755f00d25a1939cc7d9f77915dec3e0
Ian Gilmore <ian.gilmore@me.com>
2024-05-15 11:31:34 -0400

    update settings.py for deployment

--- projects/settings.py
+++ projects/settings.py
@@ -37,10 +37,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aly402xftto+)!@n_vij%zwgduym42q174dn!wcuaer7av%&sz'
+# SECRET_KEY = 'django-insecure-aly402xftto+)!@n_vij%zwgduym42q174dn!wcuaer7av%&sz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
