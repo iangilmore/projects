ae0b089c33cb18cd1b306bccdddf36500c88e6c3
Ian Gilmore <ian.gilmore@me.com>
2024-05-15 07:18:08 -0400

    add User as foreign key field to Project model so we can associate each project with a specific user.

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Language(models.Model):
     name = models.CharField(max_length=50)
@@ -9,6 +9,7 @@ class Language(models.Model):
 
 
 class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
     languages = models.ManyToManyField(Language)
